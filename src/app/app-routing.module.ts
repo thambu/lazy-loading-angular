26d9e57c1ec94a789226eebc63e274793d47eb59
Rajkumar Thamb <rajkumar.thambu@gmail.com>
2018-10-31 11:26:04 +1100

    Lazy Loading Angular 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import{NgModule}from'@angular/core';
+import {Routes, RouterModule}from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'customers',
+  loadChildren: './customers/customers.module#CustomersModule'
+  },
+  {
+  path: 'orders',
+  loadChildren: './orders/orders.module#OrdersModule'
+  },
+  {
+  path: '',
+  redirectTo: '',
+  pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
